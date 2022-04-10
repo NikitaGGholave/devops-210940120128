2e7f06265e5ed91f17bc8e9e1b0e83103bc0762f
root <root@localhost.localdomain>
2022-04-10 04:51:11 -0700

    comitting my sum file of java

---
+++ sum.java
@@ -0,0 +1,9 @@
+class Sum {
+  public static void main(String args[]){
+    System.out.println("this is a simple sum program");
+    int a =10;
+    int b= 20;
+    int sum = a+b;
+    System.out.println("sum of number " +a +" "+b+"is "+sum); 
+  }
+}
